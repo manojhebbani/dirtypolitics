fe7909f41f2fa175ee78db4f37615f7e9a02b5eb
manojhebbani <hvmanojk@gmail.com>
2017-09-06 22:30:17 +0530

    mysql connect

---
+++ db/mysql.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const hbs = require("hbs");
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.set('view engine','hbs');
+app.use( express.static( __dirname + '/public' ) );
+
+const mysql = require("mysql");
+
+var conn = mysql.createConnection({
+  host :'182.50.133.86:3306',
+  user:'dirtypolitics',
+  password:'gTvz1!79',
+  database:'dirtypolitics'
+});
+
+conn.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+app.get('/',(req,res)=>{
+  res.render('posts.hbs',{
+    page  : 'posts',
+    title : 'Dirty Politics',
+    posts   : posts
+  });
+});
+app.listen(port);
