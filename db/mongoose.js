a9d63155c94c0cdac9cd24b75c3cb2d3f01126bb
manojhebbani <hvmanojk@gmail.com>
2017-09-30 10:10:46 +0530

    Revert

--- db/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/dirtypolitics",{ useMongoClient: true });
-
-// var db = mongoose.connection;
-//
-// db.on('error',console.error.bind(console,'connection error:'));
-
-module.exports = mongoose;
